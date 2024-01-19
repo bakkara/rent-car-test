d511d7dfe52a71e0c65f8b7f7ff2c60682dfc23f
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-01-19 14:41:09 +0200

    add style modal

--- src/components/ModalDetailInf/ModalDetailInf.js
+++ src/components/ModalDetailInf/ModalDetailInf.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Modal from 'react-modal'
 import { IoCloseOutline } from "react-icons/io5";
-import { ExitButton, Img, ModalContainer, customStyles } from './ModalDetailInf.styled'
-import { TextCar, TextCarSpan, TextCarWrapper, TitleTextCar } from '../CarItem/CarItem.styled';
+import { ConditionsContainer, ConditionsSpan, ExitButton, Img, ModalContainer, ModalText, ModalTextCarSpan, ModalTitleText, ModalTitleTextCar, TextCarWrapperModal, WrapperConditions, customStyles } from './ModalDetailInf.styled'
+import { TextCar, TextCarWrapper } from '../CarItem/CarItem.styled';
 import Button from '../Button/Button';
 
+// Я обирала як краще зробити модалку: зробити createPortal чи використати бібліотеку react-modal. 
+// Використала бібліотеку, так як в даному випадку в нас лише одна модалка, тому швидше і раціональніше було використати react - modal.
+// Цікавить думка досвідченого розробника.
+
 const ModalDetailInf = ({ isOpen, car, onRequestClose }) => {
-        const handleClick = () => {
-        window.location.href = 'tel:+380730000000';
-    }
-  return (
+        
+const handleClick = () => {
+    window.location.href = 'tel:+380730000000';
+}
+    const age = car.rentalConditions.split('\n')[0];
+
+    return (
       <Modal
           isOpen={isOpen}
           onRequestClose={onRequestClose}
@@ -19,8 +26,8 @@ const ModalDetailInf = ({ isOpen, car, onRequestClose }) => {
           <ModalContainer>
               <ExitButton onClick={onRequestClose}> <IoCloseOutline size={24} /></ExitButton>
               <Img src={car.img} />
-              <TitleTextCar>{car.make} <TextCarSpan>{car.model},</TextCarSpan> {car.year} </TitleTextCar>
-              <TextCarWrapper>
+              <ModalTitleTextCar>{car.make} <ModalTextCarSpan>{car.model},</ModalTextCarSpan> {car.year} </ModalTitleTextCar>
+              <TextCarWrapperModal>
                 <TextCar>{car.address.split(',')[1]}</TextCar>
                 <TextCar>{car.address.split(',')[2]}</TextCar>
                 <TextCar>Id: {car.id}</TextCar>
@@ -28,21 +35,27 @@ const ModalDetailInf = ({ isOpen, car, onRequestClose }) => {
                 <TextCar>Type: {car.type}</TextCar>
                 <TextCar>Fuel Consumption: {car.fuelConsumption}</TextCar>
                 <TextCar>Engine Size: {car.engineSize}</TextCar>
-              </TextCarWrapper>
+              </TextCarWrapperModal>
 
-              <p>{car.description}</p>
-              <p>Accessories and functionalities: </p>
+              <ModalText>{car.description}</ModalText>
+              <ModalTitleText>Accessories and functionalities: </ModalTitleText>
               <TextCarWrapper>
                 <TextCar>{car.accessories[0]}</TextCar>
                 <TextCar>{car.accessories[1]}</TextCar>
-                  <TextCar>{car.accessories[2]}</TextCar>
-                  <TextCar>{car.functionalities[0]}</TextCar>
+                <TextCar>{car.accessories[2]}</TextCar>
+                <TextCar>{car.functionalities[0]}</TextCar>
                 <TextCar>{car.functionalities[1]}</TextCar>
-               <TextCar>{car.functionalities[2]}</TextCar>
+                <TextCar>{car.functionalities[2]}</TextCar>
 
               </TextCarWrapper>
-              <p>Rental Conditions: </p>
-              
+              <ModalTitleText>Rental Conditions: </ModalTitleText>
+                <WrapperConditions>
+                    <ConditionsContainer>{age.split(':')[0]} : <ConditionsSpan> {age.split(':')[1]}</ConditionsSpan> </ConditionsContainer>
+                    <ConditionsContainer>{car.rentalConditions.split('\n')[1]}</ConditionsContainer>
+                    <ConditionsContainer>{car.rentalConditions.split('\n')[2]}</ConditionsContainer>
+                    <ConditionsContainer>Mileage: <ConditionsSpan>{car.mileage}</ConditionsSpan></ConditionsContainer>
+                    <ConditionsContainer>Price: <ConditionsSpan>{car.rentalPrice}</ConditionsSpan></ConditionsContainer>
+              </WrapperConditions>
         <Button text={"Rental car"} onClick={handleClick} width={168}/>
           
           
--- src/components/ModalDetailInf/ModalDetailInf.styled.js
+++ src/components/ModalDetailInf/ModalDetailInf.styled.js
@@ -10,20 +10,20 @@ export const customStyles = {
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
-              backgroundColor: 'rgba(229, 227, 234, 0.8)',
-              zIndex: '1200',        
+              backgroundColor: 'rgba(18, 20, 23, 0.50)',
+                zIndex: '1200', 
+              paddingTop: '20px',
+                paddingBottom: '20px',
 },
         content: {
-            top: '50%', 
-            left: '50%', 
-            right: 'auto',
-            bottom: 'auto',
-            transform: 'translate(-50%, -50%)', 
             padding: '0', 
             border: 'none', 
-            maxWidth: 'calc(100vw - 48px)',
-            maxHeight: 'calc(100vh - 24px)',
-            overflow: 'hidden',
+            width: '540px',
+            height: 'auto',
+            overflow: 'hidden auto',
+            scrollBehavior: 'smooth',
+            margin: '0 auto',
+            scrollbarWidth: 'thin',
         },
 };
 
@@ -34,7 +34,7 @@ export const ModalContainer = styled.div`
     color: ${({ theme }) => theme.colors.black};
     position: relative;
     width: 100%;
-    height: 100%;`
+    height: 100vh;`
 
 export const ExitButton = styled.button`
     position: absolute;
@@ -55,4 +55,76 @@ export const Img = styled.img`
     object-fit: cover;
     border-radius: ${({ theme }) => theme.spacing(6)};
     margin-bottom: ${({ theme }) => theme.spacing(3,5)};
+`
+export const TextCarWrapperModal = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 14px;
+    `
+
+export const ModalTitleTextCar = styled.p`
+    color: ${({ theme }) => theme.colors.dark};
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.5;
+    margin: 0;
+    margin-bottom: 8px;`
+    
+export const ModalTextCarSpan = styled.span`
+    color: ${({ theme }) => theme.colors.primary};`
+
+export const ModalTextCarWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 14px;
+    `   
+export const ModalText = styled.p`
+    color: ${({ theme }) => theme.colors.dark};
+    font-family: Manrope;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.43;
+    margin-bottom: 24px;
+`
+
+export const ModalTitleText = styled.p`
+    color: ${({ theme }) => theme.colors.dark};
+    font-family: Manrope;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1.43;
+    margin-bottom: 8px;
+`
+
+export const WrapperConditions = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+    margin-bottom: 24px;
+`
+export const ConditionsContainer = styled.div`
+    display: flex;
+    padding: 7px 14px;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    border-radius: 35px;
+    background: #F9F9F9;
+    color: #363535;
+    font-family: Montserrat;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.5;
+    letter-spacing: -0.24px;
+`
+
+export const ConditionsSpan = styled.span`
+    color: #3470FF;
+    font-family: Montserrat;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 18px;
+    letter-spacing: -0.24px;
 `
